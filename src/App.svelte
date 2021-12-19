6faea6de369102bd25f30ed5d8a382abe824bc0d
ShchMax-sudo <Sherbonos.maksim@yandex.ru>
2021-12-19 18:14:48 +0300

    Working todos without auth

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,51 @@
 	let addTodo = () => {
 		todosStore.addTodo({
 			text: addTodoText,
-		}).then(() => console.log(
-			"Added todo"
-		));
+		});
 	}
-
+	let deleteTodo = (id) => {
+		todosStore.deleteTodo(id);
+	}
+	let doneTodo = (todo) => {
+		todosStore.changeTodo({
+			editing: todo.editing,
+			done: !todo.done,
+			text: todo.text,
+			id: todo.id,
+		});
+	}
+	let editTodo = (todo) => {
+		todosStore.changeTodo({
+			editing: !todo.editing,
+			done: todo.done,
+			text: todo.text,
+			id: todo.id,
+		});
+	}
+	let saveTodo = (todo) => {
+		todosStore.changeTodo({
+			editing: !todo.editing,
+			done: todo.done,
+			text: todo.newtext,
+			id: todo.id,
+		});
+	}
 </script>
 
 <ul>
 	{#each $todosStore as todo (todo.id)}
-		<li>{todo.text}</li>
+		<li>
+			<input type="checkbox" checked={todo.done} on:click={doneTodo(todo)}>
+			{#if todo.editing}
+			<input bind:value={todo.newtext}>
+			<button on:click={saveTodo(todo)}>Save</button>
+			<button on:click={editTodo(todo)}>Discard</button>
+			{:else}
+			{todo.text}
+			<button on:click={editTodo(todo)}>Edit</button>
+			<button on:click={deleteTodo(todo.id)}>Delete</button>
+			{/if}
+		</li>
 	{/each}
 	<input bind:value={addTodoText}> <button on:click={addTodo}>Add todo</button>
 </ul>
